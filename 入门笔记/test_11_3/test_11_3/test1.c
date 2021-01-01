3dd7e1a47e12be04c2606f538dd619d4c619d5d5
YiZhiFei <364815799@qq.com>
2021-01-01 18:51:11 +0800

    2021.1.1更新

--- 入门笔记/test_11_3/test_11_3/test1.c
+++ 入门笔记/test_11_3/test_11_3/test1.c
@@ -17,9 +17,10 @@ int main()
    return 0;
 }*/
 
+//常量
 //int main()
 //{
-//	//const - 常属性
+//2.const - 常属性
 //	const int n = 10;//n是变量，但是又有常属性，所以我们说n是常变量
 //	//int arr[n]={0};//无法编译
 //	//n = 20;//无法编译
@@ -33,6 +34,7 @@ int main()
 //	100;
 //	3.14;*/
 //}
+
 //3. #define 定义的标识符常量
 //#define MAX 10
 //int main()
@@ -41,6 +43,7 @@ int main()
 //	printf("%d\n",MAX);
 //	return 0;
 //}
+
 //4.枚举常量
 //枚举一一列举
 //性别：男，女，保密
@@ -48,17 +51,65 @@ int main()
 
 //枚举关键 -enum
 
-enum Sex
-{
-	MALE;
-	FEMALE;
-	SECRET;
-};  
-//MALE,FEMALE,SECRET - 枚举常量
-int main()
-{
-	printf("%d\n",MALE);
-	printf("%d\n",FEMALE);
-	printf("%d\n",SECRET);
-}
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+////MALE,FEMALE,SECRET - 枚举常量
+//int main()
+//{
+//	/*enum Sex s = FEMALE;*/
+//	printf("%d\n",MALE);
+//	printf("%d\n",FEMALE);
+//	printf("%d\n",SECRET);
+//	return 0;
+//}
+//练一练
+//enum Color
+//{
+//	RED,
+//	YELLOW,
+//	BLUE
+//};
+//int main()
+//{
+//    /*enum Color c = BLUE;   c是变量*/
+//	printf("%d\n",RED);
+//	printf("%d\n",YELLOW);
+//	printf("%d\n",BLUE);
+//	return 0;
+//}
 
+//字符串类型
+//int main()
+//{
+//	"abcdef";
+//	"hello bit";
+//	"";//空字符串
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] ="abc";//数组  "abc"--'a','b','c'，'\0'字符串的结束标志
+//	char arr2[] ={'a','b','c'，0};//出现乱码
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] ="abc";
+//	char arr2[] ={'a','b','c'};
+//	printf("%d\n",strlen(arr1));//string length-计算字符串的长度
+//	printf("%d\n",strlen(arr2));//随机值
+//	return 0;
+//}
+
+//转义字符
+//\n 换行
+//\t 水平制表符
+//\\ 让斜杠变成普通斜杠 \'同理
---
+++ 入门笔记/test_11_3/test_11_3/test2.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	printf("%d\n",strlen("c:\test\32\test.c"));
+//	//\32 -- 32是2个8进制数字
+//	//32作为8进制代表的那个十进制数字，作为ASCII码值
+//	//32---8进制为26--ASCII码为向右的箭头
+//	//\ddd ddd为八进制数
+//	//\xdd   dd为十六进制
+//}
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特！\n");
+//	
+//	while(line<20000)
+//	{
+//		printf("请打出一行代码\n");
+//		line++;
+//
+//	}
+//	printf("拥有好的offer\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特！\n");
+//	printf("你要好好学习吗 <(1/0)\n");
+//	scanf("%d",&input);
+//	if(input = 1)
+//		printf("好的offer\n");
+//	else
+//		printf("卖红薯\n");
+//
+//	return 0;
+//}
+
+////函数
+//int Add(int x,int y)
+//{
+//	int z = x+y;
+//	return z;
+//}
+//int main()
+//{
+//	int sum = 0;
+//	int num1 = 10;
+//	int num2 = 20;
+//	int a = 100;
+//	int b = 200;
+//	sum = Add(num1,num2);
+//	sum = Add(a,b);
+//	printf("sum = %d\n",sum);
+//	return 0;
+//}
+
+//算术操作符
+
+//移位操作符
+//移二进制操作符
+//<<左移 >>you
+
+//位操作符
+//赋值操作符
+//单目操作符
+//双目操作符
+//三目操作符
+
+
+
+
+
+
+
